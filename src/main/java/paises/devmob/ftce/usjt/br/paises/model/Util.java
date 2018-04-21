2ec1f85b729e30704dedd3e52e3c7628892594d3
Thiagobarbosa28 <thg.barbosa28@gmail.com>
2018-04-21 01:07:03 -0300

    Initial commit

---
+++ src/main/java/paises/devmob/ftce/usjt/br/paises/model/Util.java
@@ -0,0 +1,27 @@
+package paises.devmob.ftce.usjt.br.paises.model;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import java.lang.reflect.Field;
+
+import paises.devmob.ftce.usjt.br.paises.R;
+/**
+ * Created by Thiago Barbosa 816117515
+ * */
+
+public class Util {
+    public static Drawable getDrawableDinamic(Context contexto, String nomeImagem){
+        Class<?> c = R.drawable.class;
+        try {
+            Field idField = c.getDeclaredField(nomeImagem);
+            int id = idField.getInt(idField);
+            return contexto.getResources().getDrawable(id, null);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
